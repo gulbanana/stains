8a2d74f192faa7382c1267b195686a5101c75d3e
Thomas Castiglione <castiglione@mac.com>
2021-11-22 11:00:15 +0800

    mess around with colour a bit to demonstrate a smooth animation

---
+++ Pulse.cs
@@ -0,0 +1,31 @@
+﻿namespace Stains
+{
+    struct Pulse
+    {
+        public double Value;
+        private bool decreasing;
+
+        public void Tick(double seconds)
+        {
+            var diff = seconds % 2.0;
+            if (!decreasing)
+            {
+                Value += diff;
+                if (Value > 1.0)
+                {
+                    decreasing = true;
+                    Value -= (Value - 1.0);
+                }
+            }
+            else
+            {
+                Value -= diff;
+                if (Value < 0.0)
+                {
+                    decreasing = false;
+                    Value += (0.0 - Value);
+                }
+            }
+        }
+    }
+}
